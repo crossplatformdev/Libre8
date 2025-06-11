b21ad852f258e05b66d0a6df8a2201aa083b1bfa
Elías A. Angulo Klein <elias.angulo.klein@gmail.com>
2025-06-11 20:05:01 +0200

    Examples moved.

--- toolchain/src/org/elijaxapps/example/StartHere.java
+++ toolchain/src/org/elijaxapps/example/StartHere.java
@@ -1,6 +1,5 @@
 package org.elijaxapps.libre8;
-import org.elijaxapps.libre8.assembler.AssemblerV8;
-import org.elijaxapps.libre8.compiler.MiniCCompiler;
+
 public class StartHere {
 
     public static void main(String[] args) throws Exception {
